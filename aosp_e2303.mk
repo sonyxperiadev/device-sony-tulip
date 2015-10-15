1e29d2809365f28b76af21712dd0a47fa4e82ef6
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-10-15 15:35:54 +0200

    tulip: make 64-bit primary, 32-bit secondary
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_e2303.mk
+++ aosp_e2303.mk
@@ -17,7 +17,11 @@ TARGET_KERNEL_CONFIG := aosp_kanuti_tulip_defconfig
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/tulip/overlay
 
+# Use zygote64_32
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/kanuti/device.mk)
 $(call inherit-product, vendor/sony/tulip/tulip-vendor.mk)
