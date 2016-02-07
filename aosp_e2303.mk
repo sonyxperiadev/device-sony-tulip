6398d52a15fe3d7994308f5c50016f44d38fee27
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-07 21:50:22 +0100

    add core_64_bit.mk
    according to AOSP
    https://android.googlesource.com/platform/build/+/master/target/product/core_64_bit.mk#17
    
    which means that this has to be placed before
    product/aosp_base.mk or product/aosp_base_telephony.mk calls

--- aosp_e2303.mk
+++ aosp_e2303.mk
@@ -17,6 +17,7 @@ TARGET_KERNEL_CONFIG := aosp_kanuti_tulip_defconfig
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/tulip/overlay
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/sony/kanuti/device.mk)
 $(call inherit-product, vendor/sony/tulip/tulip-vendor.mk)
