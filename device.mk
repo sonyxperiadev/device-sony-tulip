fb227b95500c60530864b2f2d291874f6798297b
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-05 12:09:08 +0200

    include optional vendor configuration
    optional configuration is needed to generate the vendor zip
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- device.mk
+++ device.mk
@@ -64,3 +64,6 @@ $(call inherit-product, vendor/sony/kanuti-tulip/tulip-vendor.mk)
 
 # copy NFC firmware
 $(call inherit-product-if-exists, vendor/nxp/nxp-vendor.mk)
+
+# include optional vendor configuration
+$(call inherit-product-if-exists, vendor/qcom/proprietary/common/build/qcom-packages.mk)
