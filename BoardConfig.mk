sonyxperiadev/device-sony-kugo

declare PRODUCT_PLATFORM
PRODUCT_PLATFORM is used as build barier on all projects and
should be defined for each device

Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

BoardConfig.mk
@@ -16,6 +16,9 @@ include device/sony/loire/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := F5321
 
+# Platform
+PRODUCT_PLATFORM := loire
+
 BOARD_KERNEL_CMDLINE += androidboot.hardware=kugo
 
 # Recovery config

sonyxperiadev/device-sony-tulip

BoardConfig.mk
@@ -16,6 +16,9 @@ include device/sony/kanuti/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := E2303
 
+# Platform
+PRODUCT_PLATFORM := kanuti
+
 BOARD_KERNEL_CMDLINE += androidboot.hardware=tulip
 
 # Recovery config