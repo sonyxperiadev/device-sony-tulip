sonyxperiadev/device-sony-suzu

update TARGET_BOOTLOADER_BOARD_NAME
TARGET_BOOTLOADER_BOARD_NAME is common for all devices that are built on
the same board and should be declared as platform name

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

BoardConfig.mk
@@ -14,7 +14,7 @@
 
 include device/sony/loire/PlatformConfig.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := F5121
+TARGET_BOOTLOADER_BOARD_NAME := loire
 
 # Platform
 PRODUCT_PLATFORM := loire

sonyxperiadev/device-sony-tulip

BoardConfig.mk
@@ -14,7 +14,7 @@
 
 include device/sony/kanuti/PlatformConfig.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := E2303
+TARGET_BOOTLOADER_BOARD_NAME := kanuti
 
 # Platform
 PRODUCT_PLATFORM := kanuti